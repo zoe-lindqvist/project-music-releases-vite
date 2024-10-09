abda679ca48e93e87a864a3a1d988a06baa5191f
Zoe <zoe.lindqvist@gmail.com>
2024-10-09 18:13:40 +0200

    responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import data from "./data.json"; // Spotify data
 import { Header } from "./components/Header/Header.jsx";
 import { Album } from "./components/Album/Album.jsx";
+import "./App.css";
+// import "./Album.css";
 
 export const App = () => {
   return (
     <>
       <Header />
-      <div>
+
+      <div className="album-grid">
         {/* Loop through each album in the data */}
         {data.albums.items.map((album) => (
           <Album key={album.id} album={album} />
--- src/components/Album/Album.jsx
+++ src/components/Album/Album.jsx
@@ -1,11 +1,28 @@
-import { PropTypes } from "prop-types";
-import { ArtistName } from "../ArtistName"; // Import ArtistName component
+import PropTypes from "prop-types";
+import { ArtistName } from "../ArtistName";
+import heartIcon from "../../assets/icons/heart.svg";
+import playIcon from "../../assets/icons/play.svg";
+import dotsIcon from "../../assets/icons/dots.svg";
+import "./Album.css";
 
 export const Album = ({ album }) => {
   return (
     <div className="album">
-      {/* Album cover image */}
-      <img src={album.images[0].url} alt={album.name} className="album-cover" />
+      <div className="album-cover-container">
+        {/* Album cover image */}
+        <img
+          src={album.images[0].url}
+          alt={album.name}
+          className="album-cover"
+        />
+
+        {/* Icons overlay */}
+        <div className="icons">
+          <img src={heartIcon} alt="Favorite" className="icon" />
+          <img src={playIcon} alt="Play" className="icon play-icon" />
+          <img src={dotsIcon} alt="More options" className="icon" />
+        </div>
+      </div>
 
       {/* Album name */}
       <h3 className="album-name">
@@ -47,9 +64,6 @@ Album.propTypes = {
       PropTypes.shape({
         id: PropTypes.string.isRequired,
         name: PropTypes.string.isRequired,
-        external_urls: PropTypes.shape({
-          spotify: PropTypes.string.isRequired,
-        }).isRequired,
       })
     ).isRequired,
   }).isRequired,
