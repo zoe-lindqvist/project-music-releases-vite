b18b1272b4de8f9b2c8df9ca695aaff2fcefc2c5
Zoe <zoe.lindqvist@gmail.com>
2024-10-09 20:26:46 +0200

    video

--- src/components/Album/Album.jsx
+++ src/components/Album/Album.jsx
@@ -1,6 +1,5 @@
 import PropTypes from "prop-types";
 import { ArtistName } from "../ArtistName/ArtistName.jsx";
-import heartIcon from "../../assets/icons/heart.svg";
 import playIcon from "../../assets/icons/play.svg";
 import dotsIcon from "../../assets/icons/dots.svg";
 import "./Album.css";
@@ -18,8 +17,33 @@ export const Album = ({ album }) => {
 
         {/* Icons overlay */}
         <div className="icons">
-          <img src={heartIcon} alt="Favorite" className="icon" />
-          <img src={playIcon} alt="Play" className="icon play-icon" />
+          {/* Hidden checkbox to toggle heart color */}
+          <input
+            type="checkbox"
+            id={`heart-${album.id}`}
+            className="heart-checkbox"
+          />
+
+          {/* Label for heart icon, referencing checkbox */}
+          <label
+            htmlFor={`heart-${album.id}`}
+            className="icon heart-icon"
+          ></label>
+
+          {/* Play Icon */}
+          <a
+            href={album.external_urls.spotify}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={playIcon}
+              alt="Play on Spotify"
+              className="icon play-icon"
+            />
+          </a>
+
+          {/* More options icon */}
           <img src={dotsIcon} alt="More options" className="icon" />
         </div>
       </div>
@@ -51,6 +75,7 @@ export const Album = ({ album }) => {
 
 Album.propTypes = {
   album: PropTypes.shape({
+    id: PropTypes.string.isRequired, // Ensure `id` is defined here
     name: PropTypes.string.isRequired,
     external_urls: PropTypes.shape({
       spotify: PropTypes.string.isRequired,
