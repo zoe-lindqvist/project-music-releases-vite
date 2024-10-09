edb9710e81fc788498fa11c5d924d8eb9747e07f
Zoe <zoe.lindqvist@gmail.com>
2024-10-09 15:25:05 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import data from "./data.json"; //Spotify data
-import Album from "./components/Album";
-
-console.log(data);
+import "./data.json"; //Spotify data
+// import { Album } from "./components/Album";
+import { Header } from "./components/Header/Header.jsx";
 
 export const App = () => {
   return (
-    <div className="album-list">
-      {/* Map through the data and render an Album component for each album */}
-      {data.albums.items.map((album) => (
-        <Album key={album.id} album={album} />
-      ))}
-    </div>
+    <>
+      <Header></Header>
+    </>
+
+    // <div className="album-list">
+    //   {/* Map through the data and render an Album component for each album */}
+    //   {data.albums.items.map((album) => (
+    //     <Album key={album.id} album={album} />
+    //   ))}
+    // </div>
   );
 };
-
-export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "./styling/Header.css";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <h1>Musify</h1>
+      <p>Your music releases</p>
+    </header>
+  );
+};
